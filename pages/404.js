44a5f49a999fb8126861b8b69440fde54fceb60b
AlexNix <alexnix.mail@gmail.com>
2022-06-15 23:29:36 +0300

    design-notFound-page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Router from "next/router";
+import styles from "../styles/NotFound.module.scss";
 
 const NotFound = () => {
-	return <div>NotFound</div>;
+	return (
+		<div className={styles.container}>
+			<h1>
+				<span>404</span>
+				<br />
+				Page couldn't be found
+			</h1>
+			<p>
+				The requested page either doesn't exist or you don't have access to it.
+			</p>
+			<a onClick={() => Router.back()}>Go Back</a>
+		</div>
+	);
 };
 
 export default NotFound;
