304f8b760437e1babcb3e0b0b5bec3fe0c7e40ab
AlexNix <alexnix.mail@gmail.com>
2022-06-29 13:52:58 +0300

    change-carousel

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,52 +1,73 @@
-import { motion } from "framer-motion";
-import { useRef, useState, useEffect } from "react";
-import styles from "../styles/Project.module.scss";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import Link from "next/link";
-
+import styles from "../styles/Project.module.scss";
+import { FaDotCircle, FaRegDotCircle } from "react-icons/fa";
 import { urlFor } from "../lib/client";
 
 const Project = ({ projects, title = "What I've been working on" }) => {
-	const [width, setWidth] = useState(0);
-	const carousel = useRef();
-	useEffect(() => {
-		setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-	}, []);
-
+	const responsive = {
+		desktop: {
+			breakpoint: { max: 3000, min: 1200 },
+			items: 3,
+			slidesToSlide: 1,
+		},
+		tablet: {
+			breakpoint: { max: 1200, min: 495 },
+			items: 2,
+			slidesToSlide: 1,
+		},
+		mobile: {
+			breakpoint: { max: 495, min: 0 },
+			items: 1,
+			slidesToSlide: 1,
+		},
+	};
+	const CustomDot = ({ onMove, index, onClick, active }) => {
+		return (
+			<li
+				className={active ? `${styles.active}` : `${styles.inactive}`}
+				onClick={() => onClick()}
+			>
+				{active ? <FaDotCircle /> : <FaRegDotCircle />}
+			</li>
+		);
+	};
 	return (
 		<article className={styles.conteiner}>
 			<h1>{title}</h1>
-			<motion.div
-				ref={carousel}
-				className={styles.carousel}
-				whileTap={{ cursor: "grabbing" }}
+			<Carousel
+				responsive={responsive}
+				infinite
+				showDots
+				ssr
+				className={styles.slider_container}
+				arrows={false}
+				autoPlay={true}
+				autoPlaySpeed={7000}
+				customDot={<CustomDot />}
 			>
-				<motion.div
-					drag="x"
-					dragConstraints={{ right: 0, left: -width }}
-					className={styles.inner_carousel}
-				>
-					{projects?.map((project) => {
-						return (
-							<div key={project._id} className={styles.project_cart}>
-								<div className={styles.content}>
-									<img
-										src={urlFor(project?.title_img)}
-										className={styles.img_cart}
-										alt="title-img"
-									/>
-									<h1>{project.project_name}</h1>
-									<p>{project.short_description}</p>
-								</div>
-								<div className={styles.link}>
-									<Link href={`/project/${project.slug.current}`}>
-										<a>VIEW PROJECT ›</a>
-									</Link>
-								</div>
+				{projects?.map((project) => {
+					return (
+						<div key={project._id} className={styles.project_cart}>
+							<div className={styles.content}>
+								<img
+									src={urlFor(project?.title_img)}
+									className={styles.img_cart}
+									alt="title-img"
+								/>
+								<h1>{project.project_name}</h1>
+								<p>{project.short_description}</p>
 							</div>
-						);
-					})}
-				</motion.div>
-			</motion.div>
+							<div className={styles.link}>
+								<Link href={`/project/${project.slug.current}`}>
+									<a>VIEW PROJECT ›</a>
+								</Link>
+							</div>
+						</div>
+					);
+				})}
+			</Carousel>
 		</article>
 	);
 };
